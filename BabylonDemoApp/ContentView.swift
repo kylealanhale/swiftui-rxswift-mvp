879e3c0ed13a8d8bee6f62cb36a8820d42240cf5
Kyle Alan Hale <kylealanhale@gmail.com>
2019-06-27 15:27:44 -0400

    Initial Commit

---
+++ BabylonDemoApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BabylonDemoApp
+//
+//  Created by Kyle Alan Hale on 6/27/19.
+//  Copyright © 2019 Kyle Alan Hale. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
