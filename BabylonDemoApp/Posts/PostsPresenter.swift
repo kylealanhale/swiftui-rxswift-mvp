6ae8fde6a1c2ec040b38cc6dedb02e1bdea8b525
Kyle Alan Hale <kylealanhale@gmail.com>
2019-07-08 01:43:59 -0400

    refactor presenter to look (and parse in Xcode) better

--- BabylonDemoApp/Posts/PostsPresenter.swift
+++ BabylonDemoApp/Posts/PostsPresenter.swift
@@ -27,48 +27,56 @@ protocol PostsPresenter: BindableObject {
 }
 
 final class ProductionPostsPresenter: PostsPresenter {
-    var didChange = PassthroughSubject<Void, Never>()
-    
     var items: [PostsListItem] = []
     var errorMessage: String? = nil
     
+    private let interactor: PostsInteractor
+    
     init(interactor: PostsInteractor) {
-        self.populate(interactor: interactor)
+        self.interactor = interactor
+        self.populate()
     }
     
-    private func populate(interactor: PostsInteractor) {
+    private func populate() {
         interactor.getPosts()
-            // Get details associated with each post, preserving order for later sorting
-            .flatMap { posts in
-                // Take first 1000 posts only, since this implementation lacks paging
-                Observable.merge(posts.prefix(1000).enumerated().map { index, post in
-                    Observable.zip(
-                        Observable.just(index),
-                        Observable.just(post),
-                        interactor.getUser(userId: post.userId),
-                        interactor.getComments(postId: post.id).map { $0.count }
-                    )
-                } )
-                // Collect results in a new list
-                .buffer(timeSpan: RxTimeInterval.seconds(Int.max), count: posts.count, scheduler: MainScheduler.instance).take(1)
-                // Sort and convert to view model type
-                .map { $0
-                    .sorted(by: { $0.0 < $1.0 })
-                    .map { (_, post, user, commentCount) in
-                        PostsListItem(id: post.id, title: post.title, author: user.name, description: post.body, commentCount: commentCount) }
-                }
-            }
+            .flatMap { posts in  self.mergeDetails(posts) }
             .subscribeOn(MainScheduler.instance)
             .subscribe(onNext: { [weak self] items in
                 self?.items = items
                 self?.errorMessage = nil
                 self?.didChange.send()
-                }, onError: { [weak self] error in
-                    self?.errorMessage = "Could not retrieve new posts"
-                    self?.didChange.send()
+            }, onError: { [weak self] error in
+                self?.errorMessage = "Could not retrieve new posts"
+                self?.didChange.send()
             })
             .disposed(by: self.disposeBag)
     }
     
+    // Get details associated with each post, preserving order for later sorting
+    private func mergeDetails(_ posts: [Post]) -> Observable<[PostsListItem]> {
+        // Take first 1000 posts only, since this implementation lacks paging
+        let posts = posts.prefix(1000)
+        let bufferScheduler = SerialDispatchQueueScheduler(qos: .userInitiated)
+
+        return Observable.merge(posts.enumerated().map { index, post in
+            // Get the user and comments for each post
+            Observable.zip(
+                Observable.just(index),
+                Observable.just(post),
+                interactor.getUser(userId: post.userId),
+                interactor.getComments(postId: post.id).map { $0.count }
+            )
+        })
+        // Collect results in a new list
+        .buffer(timeSpan: RxTimeInterval.seconds(Int.max), count: posts.count, scheduler: bufferScheduler).take(1)
+        // Sort and convert to view model type
+        .map { $0
+            .sorted(by: { $0.0 < $1.0 })
+            .map { (_, post, user, commentCount) in
+                PostsListItem(id: post.id, title: post.title, author: user.name, description: post.body, commentCount: commentCount) }
+        }
+    }
+
+    var didChange = PassthroughSubject<Void, Never>()
     private let disposeBag = DisposeBag()
 }
